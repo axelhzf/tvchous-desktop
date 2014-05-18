049142493b4fb67f27cdc2907f3c1e1bd6861bbe
Axel Hernández Ferrera <axelhzf@gmail.com>
2014-05-18 22:37:20 +0100

    Priority to 720p

--- client/js/controller/SeasonController.js
+++ client/js/controller/SeasonController.js
@@ -24,13 +24,20 @@ angular.module("app").controller("SeasonController",
 
     function downloadEpisode (episode) {
       co(function *() {
-        if (!episode.torrents) { //TODO duplicated code in EpisodeController
+        if (!episode.torrents) {
           var q = episode._show.id + " " + episode.fullId;
           episode.torrents = yield pirateshipService.findTorrents(q);
         }
-        var torrent = episode.torrents[0].link;
-        yield execService.downloadTorrent(torrent);
+        var torrent = findHdTorrent(episode.torrents) || episode.torrents[0];
+        console.log("Downloading torrent", torrent.title);
+        yield execService.downloadTorrent(torrent.link);
       })();
+    }
+
+    function findHdTorrent(torrents) {
+      return _.find(torrents, function (torrent) {
+        return torrent.title.toLowerCase().indexOf("720p") !== -1;
+      });
     }
 
     _.extend($scope, {
