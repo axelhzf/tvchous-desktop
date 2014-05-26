383dafaec8542cf25811254e42440332fb2fad41
Axel Hernández Ferrera <axelhzf@gmail.com>
2014-05-26 18:32:44 +0100

    streaming

--- app/client/js/controller/SeasonController.js
+++ app/client/js/controller/SeasonController.js
@@ -20,23 +20,36 @@ angular.module("app").controller("SeasonController",
 
     function playEpisode (episode) {
       co(function *() {
+        $scope.loadingMsg = "Opening file";
         var file = episode.local.mountFile || episode.local.file;
         yield execService.playFile(file);
+        delete $scope.loadingMsg;
       })();
     }
 
     function downloadEpisode (episode) {
       co(function *() {
+        $scope.loadingMsg = "Downloading torrent";
         var torrent = yield torrents.defaultTorrentForEpisode(episode);
         yield execService.downloadTorrent(torrent.link);
+        delete $scope.loadingMsg;
       })();
     }
 
     function streamEpisode (episode) {
+      console.log("stream episode", episode);
       co(function* () {
+        $scope.loadingMsg = "Streaming torrent";
         var torrent = yield torrents.defaultTorrentForEpisode(episode);
         var href = yield remote.startStreamingTorrent(torrent.link);
-        mediaPlayer.playRemote(href);
+        mediaPlayer.playRemote(href, stopStreaming);
+        delete $scope.loadingMsg;
+      })();
+    }
+
+    function stopStreaming () {
+      co(function* () {
+        yield remote.stopStreamingTorrent();
       })();
     }
 
